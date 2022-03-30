866673e982aed1b34e813eedaabb96533967b62c
Rafael Tivane <luislangabusiness@gmail.com>
2022-03-30 08:10:05 +0200

    make header tablet version responsive

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,16 +4,26 @@ import { Hidden, IconButton } from '@mui/material'
 import classNames from 'classnames'
 import logo from '../../../public/images/shared/desktop/logo.svg'
 import Image from 'next/image'
+import { useMemo } from 'react'
 
 const Header = () => {
+
+    const menu = useMemo(() => (
+        <IconButton className={classNames('sm:mr-8')}>
+            <MenuIcon className="text-slate-50" />
+        </IconButton>
+    ), []);
 
     return (
         <header className={classNames(`flex items-center justify-between text-slate-50 px-[5%] bg-black
             py-3`)}>
-            <IconButton>
-                <MenuIcon className="text-slate-50" />
-            </IconButton>
-            <div>
+            <Hidden smUp>
+                { menu }
+            </Hidden>
+            <div className={classNames('flex items-center')}>
+                <Hidden smDown mdUp>
+                    { menu }
+                </Hidden>
                 <Image 
                     alt="logo"
                     src={logo}
