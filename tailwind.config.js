580f173661016325714c9f5b91cc1a6151475df6
Rafael Tivane <luislangabusiness@gmail.com>
2022-03-30 07:34:33 +0200

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,36 @@ module.exports = {
   ],
   theme: {
     extend: {
-      
+      colors: {
+        brown: {
+          400: '#fbaf85',
+          500: '#D87D4A'
+        },
+        gray: {
+          200: "#FAFAFA",
+          300: '#F1F1F1'
+        },
+        zinc: {
+          800: '#101010'
+        }
+      }
     },
+    screens: {
+      'sm': '600px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '900px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
   },
   plugins: [],
 }
