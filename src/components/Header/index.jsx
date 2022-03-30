7c88c0afe59a8e57e9f66941a7ecc6a13c1f0b0e
Rafael Tivane <luislangabusiness@gmail.com>
2022-03-30 07:52:24 +0200

    add icons in header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,21 @@
-
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Hidden, IconButton } from '@mui/material'
+import classNames from 'classnames'
+import { ReactComponent as Logo } from '../../../public/images/shared/desktop/logo.svg'
 
 const Header = () => {
     return (
-        <header>
-            <h1>hello</h1>
+        <header className={classNames(`flex items-center justify-between text-slate-50 bg-black`)}>
+            <IconButton>
+                <MenuIcon />
+            </IconButton>
+            <div>
+                
+            </div>
+            <IconButton>
+                <ShoppingCartOutlinedIcon />
+            </IconButton>
         </header>
     )
 };
