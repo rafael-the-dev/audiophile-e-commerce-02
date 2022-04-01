2cea157e85e829ed254a4cc3c09571beb52b9db9
Rafael Tivane <luislangabusiness@gmail.com>
2022-04-01 07:18:54 +0200

    create shared section - best gear

---
+++ src/components/BestGear/index.jsx
@@ -0,0 +1,32 @@
+import classNames from 'classnames';
+import { Hidden, Typography } from '@mui/material'
+import Image from 'next/image'
+import bestGearImage from '../../../public/images/shared/mobile/image-best-gear.jpg'
+import bestGearTabletImage from '../../../public/images/shared/tablet/image-best-gear.jpg'
+import bestGearDesktopImage from '../../../public/images/shared/desktop/image-best-gear.jpg'
+import Paragraph from '../Paragraph'
+
+const BestGear = () => {
+    return (
+        <section className={classNames(`px-[5%]`)}>
+            <Hidden>
+                <Image 
+                    alt='best gear'
+                    src={bestGearImage}
+                />
+            </Hidden>
+            <div className='text-center'>
+                <Paragraph>
+                    Located at the heart of New York City, Audiophile is the premier 
+                    store for high end headphones, earphones, speakers, and audio accessories. 
+                    We have a large showroom and luxury demonstration rooms available for you 
+                    to browse and experience a wide range of our products. Stop by our store to 
+                    meet some of the fantastic people who make Audiophile the best place to buy your 
+                    portable audio equipment.
+                </Paragraph>
+            </div>
+        </section>
+    );
+};
+
+export default BestGear;
