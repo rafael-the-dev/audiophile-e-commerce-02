89b5a2ee7440e57cf33a667284af90f06cc45469
Rafael Tivane <luislangabusiness@gmail.com>
2022-04-08 07:58:47 +0200

    create static genaration methods

--- src/pages/[category]/[id]/index.jsx
+++ src/pages/[category]/[id]/index.jsx
@@ -1,8 +1,38 @@
 import Link from 'next/link'
 import { Button } from '@mui/material'
 import CategoriesPreview from '../../../components/CategoriesPreview';
+import data from "../../../../public/data.json"
 
-const Container = () => {
+export const getStaticPaths = async () => {
+    const paths = data.reduce((prevValue, currentValue) => {
+        const getParam = (id) => ({ params: { category: currentValue.category, id } });
+
+        return [ ...prevValue, getParam(String(currentValue.id)), getParam(currentValue.slug) ]
+    }, []);
+
+    return {
+        paths,
+        fallback: true
+    };
+};
+
+export const getStaticProps = async (context) => {
+    const { id } = context.params;
+    
+    let product = data.find(item => parseInt(id) === item.id);
+
+    if(!Boolean(product)) {
+        product = data.find(item => id === item.slug);
+    }
+
+    return {
+        props: {
+            product
+        }
+    }
+};
+
+const Container = ({ product }) => {
     return (
         <>
             <div className="px-[5%] py-6">
