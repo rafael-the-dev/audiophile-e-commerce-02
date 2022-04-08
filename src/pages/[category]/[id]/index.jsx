fd0dc9ec12836dd0f0f64edc4b1f99421fba47ac
Rafael Tivane <luislangabusiness@gmail.com>
2022-04-08 07:06:37 +0200

    add categories preview, and style go back link

--- src/pages/[category]/[id]/index.jsx
+++ src/pages/[category]/[id]/index.jsx
@@ -1,16 +1,18 @@
 import Link from 'next/link'
 import { Button } from '@mui/material'
+import CategoriesPreview from '../../../components/CategoriesPreview';
 
 const Container = () => {
     return (
         <>
-            <div className="px-[5%]">
+            <div className="px-[5%] py-6">
                 <Link href="/">
-                    <a>
-                        <Button>Go back</Button>
+                    <a className='no-underline'>
+                        <Button className="capitalize">Go back</Button>
                     </a>
                 </Link>
             </div>
+            <CategoriesPreview />
         </>
     );
 };
