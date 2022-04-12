3a1b08ef77074c6011fb98065ca4e3c24c3fdde0
Rafael Tivane <luislangabusiness@gmail.com>
2022-04-12 20:43:41 +0200

    create add item action, and reducer function

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -4,3 +4,8 @@ export const addCardItems = payload => ({
     payload,
     type: "ADD_CART_ITEMS"
 });
+
+export const addCartItem = payload => ({
+    payload,
+    type: "ADD_CART_ITEM"
+})
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,6 +2,22 @@ import { addCardItems } from "../actions";
 import { initialState } from "../state";
 
 
+const addCartItemFunc = ({ action, state}) => {
+    const newItem = action.payload;
+    const list = [ ...state.cart ];
+    const newItemIndex = list.findIndex(listItem => listItem.item.id === newItem.id);
+    const hasNewItem = newItemIndex !== -1;
+
+    if(hasNewItem) {
+        list[newItemIndex].quantity += 1;
+    } else {
+        list.push({ item: newItem, quantity: 1})
+    }
+
+    return { ...state, cart: list };
+
+};
+
 export const reducer = (state=initialState, action) => {
     switch(action.type) {
         case addCardItems().type: {
