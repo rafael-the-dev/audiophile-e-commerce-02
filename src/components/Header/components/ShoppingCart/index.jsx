58fed5f9e0fa45b00a94f72e8ded1ec88908c002
Rafael Tivane <luislangabusiness@gmail.com>
2022-04-13 07:23:57 +0200

    add open modal state, create event handlers, and store component in the useMemo hook

--- src/components/Header/components/ShoppingCart/index.jsx
+++ src/components/Header/components/ShoppingCart/index.jsx
@@ -2,16 +2,38 @@ import { Badge, IconButton } from '@mui/material';
 import { useSelector } from 'react-redux'
 import { getCartLenght } from 'src/redux/selectors';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import CartModal from '../CartModal'
+import { useCallback, useMemo, useState } from 'react';
 
 const Container = () => {
     const cartLength = useSelector(getCartLenght);
+    const [ openModal, setOpenModal ] = useState(false);
 
-    return (
-        <IconButton className="pr-0">
+    const toggleModal = useCallback(() => setOpenModal(o => !o), []);
+    const handleCloseDialog = useCallback(() => setOpenModal(false), []);
+
+    const buttonMemo = useMemo(() => (
+        <IconButton 
+            className="pr-0"
+            onClick={toggleModal}>
             <Badge badgeContent={cartLength} classes={{ badge: "bg-brown-500 text-slate-100" }} showZero >
                 <ShoppingCartOutlinedIcon  className="text-slate-50" />
             </Badge>
         </IconButton>
+    ), [ cartLength ]);
+
+    const modalMemo = useMemo(() => (
+        <CartModal 
+            handleCloseDialog={handleCloseDialog} 
+            openDialog={openModal} 
+        />
+    ), [ handleCloseDialog, openModal ]);
+
+    return (
+        <>
+            { buttonMemo }
+            { modalMemo }
+        </>
     );
 };
 
