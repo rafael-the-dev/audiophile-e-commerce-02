d76172d72a63e3114b1d332d34db8827c24fba93
Rafael Tivane <luislangabusiness@gmail.com>
2022-04-13 07:50:46 +0200

    style modal and its subcomponents

--- src/components/Header/components/CartModal/index.jsx
+++ src/components/Header/components/CartModal/index.jsx
@@ -1,19 +1,24 @@
 import { useSelector } from 'react-redux'
 import { getCartLenght } from 'src/redux/selectors';
 import { Button, Dialog, Typography } from '@mui/material';
+import styles from './css/styles.module.css';
+import classNames from 'classnames'
 
 const Container = ({ handleCloseDialog, openDialog }) => {
     const cartLength = useSelector(getCartLenght);
 
     return (
-        <Dialog onClose={handleCloseDialog} open={openDialog}>
+        <Dialog 
+            classes={{ container: styles.dialogContainer, paper: classNames(styles.dialogPaper, "px-[5%] py-4"), root: styles.dialogRoot }}
+            onClose={handleCloseDialog} 
+            open={openDialog}>
             <header className='flex items-center justify-between'>
                 <Typography
-                    className="font-bold mb-8 uppercase text-2xl"
+                    className="font-bold uppercase text-2xl"
                     component="h2">
                     Cart ({ cartLength })
                 </Typography>
-                <Button className="normal-case underline">Remove all</Button>
+                <Button className="normal-case opacity-90 p-0 text-black underline">Remove all</Button>
             </header>
         </Dialog>
     );
