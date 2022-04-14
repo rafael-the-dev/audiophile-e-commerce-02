9adfd2d0e78f249457e2dd40da915b9380020e45
Rafael Tivane <luislangabusiness@gmail.com>
2022-04-14 06:36:19 +0200

    retrieve cart item by ID

--- src/redux/selectors/index.js
+++ src/redux/selectors/index.js
@@ -1,3 +1,4 @@
 export const getAllCartItems = state => state.cart;
 export const getCartLenght = state => state.cart.reduce((prevValue, currentValue) => prevValue + currentValue.quantity, 0);
 export const getTotalCartAmout = state => state.cart.reduce((prevValue, currentValue) => prevValue + currentValue.total, 0);
+export const getCartItemById = id => state => state.find(item => item.id === id);
