6c538254bd8c01b6868fd6d2fa3dd2e20c88850c
Rafael Tivane <luislangabusiness@gmail.com>
2022-04-16 08:30:18 +0200

    create local storage field, and save cart items

--- src/context/ReduxContext.jsx
+++ src/context/ReduxContext.jsx
@@ -1,11 +1,26 @@
-import { createContext } from 'react'
+import { createContext, useEffect, useRef } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { getAllCartItems } from 'src/redux/selectors'
 
 export const ReduxContext = createContext();
 ReduxContext.displayName = "ReduxContext";
 
 
 export const ReduxContextProvider = ({ children }) => {
+
+    const cart = useSelector(getAllCartItems);
+    const localStorageName = useRef("audiophile-ecommerce--cart");
+
+    useEffect(() => {
+        const data = localStorage.getItem(localStorageName.current);
+        if(!Boolean(data)) {
+            localStorage.setItem(localStorageName.current, JSON.stringify([]));
+        }
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem(localStorageName.current, JSON.stringify(cart));
+    }, [ cart ])
 
     return (
         <ReduxContext.Provider value={{}}>
