5cf2e6075b5d9a404f24b71125506c9242ef8b7e
Rafael Tivane <luislangabusiness@gmail.com>
2022-04-17 14:05:31 +0200

    add total price, grand total, and Vat

--- src/pages/checkout/index.jsx
+++ src/pages/checkout/index.jsx
@@ -1,11 +1,20 @@
 import { Grid, Paper, Typography } from '@mui/material'
 import { useSelector } from 'react-redux'
-import { getAllCartItems } from "src/redux/selectors"
+import { getAllCartItems, getTotalCartAmout } from "src/redux/selectors"
 import CheckoutItem from "src/components/CheckoutItem"
 import PayLink from "src/components/Link"
+import classNames from 'classnames'
 
 const Container = () => {
-    const cart = useSelector(getAllCartItems)
+    const cart = useSelector(getAllCartItems);
+    const totalPrice = useSelector(getTotalCartAmout)
+
+    const SubTitle = ({ className, label, value, valueClassName }) => (
+        <Typography className={classNames("flex items-center justify-between mb-3", className)}>
+            <span className="opacity-70 uppercase">{ label }</span>
+            <span className={classNames("font-bold text-xl", valueClassName)}>$ { value }</span>
+        </Typography>
+    );
     return(
         <Grid container className="px-[5%]">
             <Grid item xs={12}></Grid>
@@ -26,7 +35,11 @@ const Container = () => {
                         }
                     </div>
                     <div className="flex flex-col items-stretch pt-6">
-                        <PayLink buttonClassName="w-full" href="/" label="Continue &amp; Pay" />
+                        <SubTitle label="Total" value={totalPrice} />
+                        <SubTitle label="Shipping" value={50} />
+                        <SubTitle label="Vat (Included)" value={1079} />
+                        <SubTitle className="mt-3" label="Grand total" value={totalPrice} valueClassName="text-brown-500" />
+                        <PayLink buttonClassName="w-full" className="mt-4" href="/" label="Continue &amp; Pay" />
                     </div>
                 </Paper>
             </Grid>
