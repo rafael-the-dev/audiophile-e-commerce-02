1137433a57be1520d919f05c065d0d77b7a35e44
Rafael Tivane <luislangabusiness@gmail.com>
2022-04-17 13:11:01 +0200

    create new route - checkout page

---
+++ src/pages/checkout/index.jsx
@@ -0,0 +1,14 @@
+import { Grid } from '@mui/material'
+
+const Container = () => {
+    return(
+        <Grid container>
+            <Grid xs={12}></Grid>
+            <Grid item xs={12}>
+
+            </Grid>
+        </Grid>
+    );
+};
+
+export default Container;
