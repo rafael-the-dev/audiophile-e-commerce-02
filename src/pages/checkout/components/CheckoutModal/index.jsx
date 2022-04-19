7c3e359701d26dee91456c543506a2f016ea9e1d
Rafael Tivane <luislangabusiness@gmail.com>
2022-04-19 11:55:46 +0200

    adding link to home, and store all mini-components in useMemo

--- src/pages/checkout/components/CheckoutModal/index.jsx
+++ src/pages/checkout/components/CheckoutModal/index.jsx
@@ -1,25 +1,45 @@
 import { Dialog, DialogTitle } from '@mui/material'
 import classNames from 'classnames'
-import { useMemo } from 'react';
+import { useCallback, useMemo } from 'react';
 import Paragraph from 'src/components/Paragraph'
+import Link from 'src/components/Link'
 
 const Container = ({ handleClose, open }) => {
     console.log("dialog");
 
-    const paragraph = useMemo(() => { console.log("paragraph");
-    return (
+    const title = useMemo(() => (
+        <DialogTitle className={classNames(`p-0 text-2xl uppercase before:bg-brown-500 before:bg-center 
+            before:bg-no-repeat`)}>
+            Thank you<br/>for your order
+        </DialogTitle>
+    ), []);
+
+    const paragraph = useMemo(() => (
         <Paragraph className="mt-4">
             You will receive an email comfirmation shortly.
         </Paragraph>
-    );}, []);
+    ), []);
+    
+    const clickHandler = useCallback(() => {
+        console.log("handler")
+        handleClose();
+    }, [ handleClose ]);
+
+    const link = useMemo(() => (
+        <Link 
+            buttonClassName="w-full"
+            className="mt-4"
+            clickHandler={clickHandler} 
+            href="/" 
+            label="Back to home" 
+        />
+    ), [ clickHandler ]);
 
     return (
         <Dialog onClose={handleClose} open={open}>
-            <DialogTitle className={classNames(`p-0 text-2xl uppercase before:bg-brown-500 before:bg-center 
-            before:bg-no-repeat`)}>
-                Thank you<br/>for your order
-            </DialogTitle>
+            { title }
             { paragraph }
+            { link }
         </Dialog>
     );
 };
